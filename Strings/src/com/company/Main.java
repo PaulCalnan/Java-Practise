e6b236c86413ae0becfcc398eb21c8c6f0672e5f
Paul Calnan <pcalnan71@gmail.com>
2022-01-04 21:34:34 +1100

    basic string operations

--- Strings/src/com/company/Main.java
+++ Strings/src/com/company/Main.java
@@ -3,6 +3,24 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        String myString = "This is a string";
+        System.out.println("myString is equal to " + myString);
+        myString = myString + ", and this is more.";
+        System.out.println("myString is  equal to " + myString);
+        myString = myString + " \u00A9 2019";
+        System.out.println("myString is equal to " + myString);
+        String numberString = "250.55";
+        numberString = numberString + "49.95";
+        System.out.println(numberString);
+        String lastString = "10";
+        int myInt = 50;
+        lastString = lastString + myInt;
+        System.out.println("LastString is equal to " + lastString);
+        double doubleNumber = 120.47d;
+        lastString = lastString + doubleNumber;
+        System.out.println("LastString is equal to " + lastString);
+//     Strings in java are immutable, you can't change a string, a new string is created when operating on a string.
+//     String buffer is used to be more efficient when appending strings
     }
 }
